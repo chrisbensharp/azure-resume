e2970fdfc98da16c8ff40546c32e7aad4caba0dc
chrisbensharp <chrisbensharp@gmail.com>
2024-09-12 15:51:55 -0500

    Adding Function App and Cosmo DB

---
+++ backend/api/Counter.cs
@@ -0,0 +1,16 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace Company.Function
+{
+
+    public class Counter
+    {
+        [JsonProperty(PropertyName="id")]
+        public string Id {get; set;}
+        [JsonProperty(PropertyName = "count")]
+        public int Count {get;set;}
+    }
+
+}
